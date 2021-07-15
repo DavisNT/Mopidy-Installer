8b7cb27970671469be1e6652edcb39d436c3baca
Davis Mosenkovs <davikovs@gmail.com>
2021-07-15 23:48:15 +0300

    Version 1.0.3
    Remove Mopidy-Simple-Webclient installation

--- rpi-install-updates.sh
+++ rpi-install-updates.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Mopidy Installer 1.0.2
+# Mopidy Installer 1.0.3
 # https://github.com/DavisNT/Mopidy-Installer
-# Copyright (c) 2016-2020 Davis Mosenkovs
+# Copyright (c) 2016-2021 Davis Mosenkovs
 # Licensed under the MIT License (see file LICENSE)
 
 sudo touch /forcefsck
@@ -18,6 +18,6 @@ sudo update-rc.d mopidy enable
 
 sudo SKIP_WARNING=1 BRANCH=stable rpi-update
 
-sudo pip3 install -U Mopidy-Mobile Mopidy-Moped Mopidy-Musicbox-Webclient Mopidy-Websettings Mopidy-Simple-Webclient Mopidy-Spotify Mopidy-Spotify-Tunigo Mopidy-Youtube Mopidy-Soundcloud Mopidy-Dirble Mopidy-Tunein Mopidy-AlarmClock Mopidy-PlaybackDefaults youtube-dl Mopidy-Iris Mopidy-Local-Sqlite
+sudo pip3 install -U Mopidy-Mobile Mopidy-Moped Mopidy-Musicbox-Webclient Mopidy-Websettings Mopidy-Spotify Mopidy-Spotify-Tunigo Mopidy-Youtube Mopidy-Soundcloud Mopidy-Dirble Mopidy-Tunein Mopidy-AlarmClock Mopidy-PlaybackDefaults youtube-dl Mopidy-Iris Mopidy-Local-Sqlite
 
 sudo sync
